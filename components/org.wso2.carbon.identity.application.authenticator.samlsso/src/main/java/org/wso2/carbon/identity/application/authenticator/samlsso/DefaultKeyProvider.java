b19dd37f533baea11a587bf6e3d5f61f02786190
shariq <msmshariq@gmail.com>
2017-05-04 14:21:49 +0530

    Update method parameters

--- components/org.wso2.carbon.identity.application.authenticator.samlsso/src/main/java/org/wso2/carbon/identity/application/authenticator/samlsso/DefaultKeyProvider.java
+++ components/org.wso2.carbon.identity.application.authenticator.samlsso/src/main/java/org/wso2/carbon/identity/application/authenticator/samlsso/DefaultKeyProvider.java
@@ -62,7 +62,7 @@ public class DefaultKeyProvider implements KeyProviderService {
     }
 
     @Override
-    public Certificate getCertificate(String s) throws Exception {
+    public Certificate getCertificate(String tenantDomain, String alias) throws Exception {
         return null;
     }
 }
